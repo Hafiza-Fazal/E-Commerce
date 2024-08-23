05f78c2431baacf1c564f58f26756c1422d35fce
Hafiza-Fazal <168233826+Hafiza-Fazal@users.noreply.github.com>
2024-08-23 19:57:48 +0500

    Add files via upload
    E-commerce website

---
+++ index.js
@@ -0,0 +1,591 @@
+
+
+var makeupProduct = {
+    Maybelline: {
+      NewYork: {
+        lip2: {
+       img:"blush1.jfif",
+        productName: "Lipstick",
+        shade: "Red",
+        price: "$10",
+      },
+      lip1: {
+        img:"eyeliner1.jfif",
+         productName: "Lips",
+         shade: "Green",
+         price: "$10",
+       },
+       lip3: {
+        img:"high.jfif",
+         productName: "cake",
+         shade: "Blue",
+         price: "$10",
+       },
+     
+    },
+      
+    turkey:{
+        lip4: {
+            img:"mas.jfif",
+             productName: "stick",
+             shade: "light pink",
+             price: "$10",
+           },
+           lip5: {
+            img:"lip.png",
+             productName: "mouse",
+             shade: "grey",
+             price: "$10",
+           },
+        
+      }
+    },
+    Estee: {
+        Germany: {
+          lip2: {
+         img:"blush2.jfif",
+          productName: "Lipstick",
+          shade: "Red",
+          price: "$10",
+        },
+        lip1: {
+          img:"eyeliner.png",
+           productName: "Lipstick",
+           shade: "Red",
+           price: "$10",
+         },
+         lip3: {
+          img:"high1.jfif",
+           productName: "Lipstick",
+           shade: "Red",
+           price: "$10",
+         },
+       
+      },
+        
+      US:{
+          lip4: {
+              img:"mas1.jfif",
+               productName: "Lipstick",
+               shade: "Red",
+               price: "$10",
+             },
+             lip5: {
+              img:"lip1.jfif",
+               productName: "Lipstick",
+               shade: "Red",
+               price: "$10",
+             },
+          
+        }
+      },
+      FentyBeauty: {
+        Japan: {
+          lip2: {
+         img:"blush3.jfif",
+          productName: "Lipstick",
+          shade: "Red",
+          price: "$10",
+        },
+        lip1: {
+          img:"eyeliner2.png",
+           productName: "Lipstick",
+           shade: "Red",
+           price: "$10",
+         },
+         lip3: {
+          img:"high2.jfif",
+           productName: "Lipstick",
+           shade: "Red",
+           price: "$10",
+         },
+       
+      },
+        
+      China:{
+          lip4: {
+              img:"mas2.jfif",
+               productName: "Lipstick",
+               shade: "Red",
+               price: "$10",
+             },
+             lip5: {
+              img:"lip2.jfif",
+               productName: "Lipstick",
+               shade: "Red",
+               price: "$10",
+             },
+          
+        }
+      },
+      NyxCosmetics: {
+       Korea: {
+          lip2: {
+         img:"blush1.jfif",
+          productName: "Lipstick",
+          shade: "Red",
+          price: "$10",
+        },
+        lip1: {
+          img:"eyeliner3.jfif",
+           productName: "Lipstick",
+           shade: "Red",
+           price: "$10",
+         },
+         lip3: {
+          img:"high3.jfif",
+           productName: "Lipstick",
+           shade: "Red",
+           price: "$10",
+         },
+       
+      },
+        
+      Russia:{
+          lip4: {
+              img:"mas3.png",
+               productName: "Lipstick",
+               shade: "Red",
+               price: "$10",
+             },
+             lip5: {
+              img:"lip3.jfif",
+               productName: "Lipstick",
+               shade: "Red",
+               price: "$10",
+             },
+          
+        }
+      },
+      LOreal: {
+        Korea: {
+           lip2: {
+          img:"blush1.jfif",
+           productName: "Lipstick",
+           shade: "Red",
+           price: "$10",
+         },
+         lip1: {
+           img:"eyeliner3.jfif",
+            productName: "Lipstick",
+            shade: "Red",
+            price: "$10",
+          },
+          lip3: {
+           img:"high3.jfif",
+            productName: "Lipstick",
+            shade: "Red",
+            price: "$10",
+          },
+        
+       },
+         
+       Russia:{
+           lip4: {
+               img:"mas3.png",
+                productName: "Lipstick",
+                shade: "Red",
+                price: "$10",
+              },
+              lip5: {
+               img:"lip3.jfif",
+                productName: "Lipstick",
+                shade: "Red",
+                price: "$10",
+              },
+           
+         }
+       },
+       Revlon: {
+        Korea: {
+           lip2: {
+          img:"blush1.jfif",
+           productName: "Lipstick",
+           shade: "Red",
+           price: "$10",
+         },
+         lip1: {
+           img:"eyeliner3.jfif",
+            productName: "Lipstick",
+            shade: "Red",
+            price: "$10",
+          },
+          lip3: {
+           img:"high3.jfif",
+            productName: "Lipstick",
+            shade: "Red",
+            price: "$10",
+          },
+        
+       },
+         
+       Russia:{
+           lip4: {
+               img:"mas3.png",
+                productName: "Lipstick",
+                shade: "Red",
+                price: "$10",
+              },
+              lip5: {
+               img:"lip3.jfif",
+                productName: "Lipstick",
+                shade: "Red",
+                price: "$10",
+              },
+           
+         }
+       },
+      Mac: {
+        Korea: {
+           lip2: {
+          img:"blush1.jfif",
+           productName: "Lipstick",
+           shade: "Red",
+           price: "$10",
+         },
+         lip1: {
+           img:"eyeliner3.jfif",
+            productName: "Lipstick",
+            shade: "Red",
+            price: "$10",
+          },
+          lip3: {
+           img:"high3.jfif",
+            productName: "Lipstick",
+            shade: "Red",
+            price: "$10",
+          },
+        
+       },
+         
+       Russia:{
+           lip4: {
+               img:"mas3.png",
+                productName: "Lipstick",
+                shade: "Red",
+                price: "$10",
+              },
+              lip5: {
+               img:"lip3.jfif",
+                productName: "Lipstick",
+                shade: "Red",
+                price: "$10",
+              },
+           
+         }
+       },
+       Nars: {
+        Korea: {
+           lip2: {
+          img:"blush1.jfif",
+           productName: "Lipstick",
+           shade: "Red",
+           price: "$10",
+         },
+         lip1: {
+           img:"eyeliner3.jfif",
+            productName: "Lipstick",
+            shade: "Red",
+            price: "$10",
+          },
+          lip3: {
+           img:"high3.jfif",
+            productName: "Lipstick",
+            shade: "Red",
+            price: "$10",
+          },
+        
+       },
+         
+       Russia:{
+           lip4: {
+               img:"mas3.png",
+                productName: "Lipstick",
+                shade: "Red",
+                price: "$10",
+              },
+              lip5: {
+               img:"lip3.jfif",
+                productName: "Lipstick",
+                shade: "Red",
+                price: "$10",
+              },
+           
+         }
+       },
+       Huda: {
+        Korea: {
+           lip2: {
+          img:"blush1.jfif",
+           productName: "Lipstick",
+           shade: "Red",
+           price: "$10",
+         },
+         lip1: {
+           img:"eyeliner3.jfif",
+            productName: "Lipstick",
+            shade: "Red",
+            price: "$10",
+          },
+          lip3: {
+           img:"high3.jfif",
+            productName: "Lipstick",
+            shade: "Red",
+            price: "$10",
+          },
+        
+       },
+         
+       Russia:{
+           lip4: {
+               img:"mas3.png",
+                productName: "Lipstick",
+                shade: "Red",
+                price: "$10",
+              },
+              lip5: {
+               img:"lip3.jfif",
+                productName: "Lipstick",
+                shade: "Red",
+                price: "$10",
+              },
+           
+         }
+       },
+       Clinique: {
+        Korea: {
+           lip2: {
+          img:"blush1.jfif",
+           productName: "Lipstick",
+           shade: "Red",
+           price: "$10",
+         },
+         lip1: {
+           img:"eyeliner3.jfif",
+            productName: "Lipstick",
+            shade: "Red",
+            price: "$10",
+          },
+          lip3: {
+           img:"high3.jfif",
+            productName: "Lipstick",
+            shade: "Red",
+            price: "$10",
+          },
+        
+       },
+         
+       Russia:{
+           lip4: {
+               img:"mas3.png",
+                productName: "Lipstick",
+                shade: "Red",
+                price: "$10",
+              },
+              lip5: {
+               img:"lip3.jfif",
+                productName: "Lipstick",
+                shade: "Red",
+                price: "$10",
+              },
+           
+         }
+       },
+       ior: {
+        Korea: {
+           lip2: {
+          img:"blush1.jfif",
+           productName: "Lipstick",
+           shade: "Red",
+           price: "$10",
+         },
+         lip1: {
+           img:"eyeliner3.jfif",
+            productName: "Lipstick",
+            shade: "Red",
+            price: "$10",
+          },
+          lip3: {
+           img:"high3.jfif",
+            productName: "Lipstick",
+            shade: "Red",
+            price: "$10",
+          },
+        
+       },
+         
+       Russia:{
+           lip4: {
+               img:"mas3.png",
+                productName: "Lipstick",
+                shade: "Red",
+                price: "$10",
+              },
+              lip5: {
+               img:"lip3.jfif",
+                productName: "Lipstick",
+                shade: "Red",
+                price: "$10",
+              },
+           
+         }
+       },
+       Dior: {
+        Korea: {
+           lip2: {
+          img:"blush1.jfif",
+           productName: "Lipstick",
+           shade: "Red",
+           price: "$10",
+         },
+         lip1: {
+           img:"eyeliner3.jfif",
+            productName: "Lipstick",
+            shade: "Red",
+            price: "$10",
+          },
+          lip3: {
+           img:"high3.jfif",
+            productName: "Lipstick",
+            shade: "Red",
+            price: "$10",
+          },
+        
+       },
+         
+       Russia:{
+           lip4: {
+               img:"mas3.png",
+                productName: "Lipstick",
+                shade: "Red",
+                price: "$10",
+              },
+              lip5: {
+               img:"lip3.jfif",
+                productName: "Lipstick",
+                shade: "Red",
+                price: "$10",
+              },
+           
+         }
+       },
+  
+  }
+
+
+  var main = document.querySelector("#main")
+  var brand = document.querySelector("#brand")
+  var country = document.querySelector("#country")
+
+  brand.innerHTML = `<option value="">Select brand </option>`
+  
+country.innerHTML = `<option value="">Select country name</option>`
+ 
+
+  
+
+
+  for(var key in makeupProduct){
+
+    brand.innerHTML += `<option value="${key}">${key}</option>`
+    for(var key1 in makeupProduct[key]){
+         
+        
+        
+        console.log(makeupProduct[key][key1]);
+    var obj = makeupProduct[key][key1]
+    
+
+    main.innerHTML +=` <div class="card text-center" style="width: 18rem;">
+            <img src="${obj.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${obj.productName}</h5>
+              <p class="card-text">${obj.price}</p>
+              <a href="#" class="btn btn-success">${obj.shade}</a>
+            </div>
+          </div>`
+          
+            
+          }
+      
+        
+    
+  }
+
+
+  function setCountry(){
+    country.innerHTML = ""
+    country.innerHTML =`<option value ="">select country name </option>`
+for(var key in makeupProduct[brand.value]){
+  country.innerHTML +=`<option value ="${key}">${key} </option>`
+  
+}
+  
+  }
+
+var selectedCard = document.getElementById("selectedCard")
+  function searchProduct(){
+    console.log(makeupProduct[brand.value][country.value]);
+  
+    var userVal = makeupProduct[brand.value][country.value]
+   
+    
+    selectedCard.innerHTML = `<div class="card text-center" style="width: 18rem;">
+            <img src="./download.jfif" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${userVal.productName}</h5>
+              <p class="card-text">${userVal.price}</p>
+              <a href="#" class="btn btn-primary">${userVal.shade}</a>
+            </div>
+          </div>`
+             main.style.display = 'none'
+          
+    
+    
+  }
+  function clearProduct (){
+    selectedCard.style.display ='none'
+    main.style.display= "flex"
+  }
+  function displayProducts() {
+    main.innerHTML = '';
+    if (brand.value && makeupProduct[brand.value]) {
+        var countryProducts = makeupProduct[brand.value][country.value];
+        if (countryProducts) {
+            for (var key in countryProducts) {
+                var obj = countryProducts[key];
+                main.innerHTML += `
+                    <div class="card text-center" style="width: 18rem;">
+                        <img src="${obj.img}" class="card-img-top" alt="${obj.productName}">
+                        <div class="card-body">
+                            <h5 class="card-title">${obj.productName}</h5>
+                            <p class="card-text">${obj.price}</p>
+                            <a href="#" class="btn btn-success">${obj.shade}</a>
+                        </div>
+                    </div>`;
+            }
+        }
+    }
+}
+function searchProduct() {
+    if (brand.value && country.value && makeupProduct[brand.value][country.value]) {
+        var userValObject = makeupProduct[brand.value][country.value];
+        selectedCard.innerHTML = '';
+        for (var key in userValObject) {
+            var userVal = userValObject[key];
+            selectedCard.innerHTML += `
+                <div class="card text-center" style="width: 18rem;">
+                    <img src="${userVal.img}" class="card-img-top" alt="${userVal.productName}">
+                    <div class="card-body">
+                        <h5 class="card-title">${userVal.productName}</h5>
+                        <p class="card-text">${userVal.price}</p>
+                        <a href="#" class="btn btn-primary">${userVal.shade}</a>
+                    </div>
+                </div>`;
+        }
+        main.style.display = 'none';
+        selectedCard.style.display = 'flex';
+    }
+}
+
+
